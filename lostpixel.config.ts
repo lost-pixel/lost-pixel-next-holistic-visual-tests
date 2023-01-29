07213482ffe35850671393d061acfc1f7bce584b
d-ivashchuk <dimaiv@me.com>
2023-01-29 16:50:13 +0100

    add storybook visual tests on lost-pixel platform

---
+++ lostpixel.config.ts
@@ -0,0 +1,10 @@
+import { CustomProjectConfig } from 'lost-pixel';
+
+export const config: CustomProjectConfig = {
+  storybookShots: {
+    storybookUrl: './storybook-static',
+  },
+
+  lostPixelProjectId: 'clde9r3rh00v3m50vlq8y0k78',
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+};
