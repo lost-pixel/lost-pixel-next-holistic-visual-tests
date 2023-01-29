414136f8eedd2bc0774708dcf97698f0c4301597
d-ivashchuk <dimaiv@me.com>
2023-01-29 17:04:59 +0100

    add page tests

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -4,7 +4,14 @@ export const config: CustomProjectConfig = {
   storybookShots: {
     storybookUrl: './storybook-static',
   },
-
+  pageShots: {
+    pages: [
+      { path: '/layouts/books/fiction', name: 'fiction-books' },
+      { path: '/layouts/books/biography', name: 'biography-pages' },
+    ],
+    // IP should be localhost when running locally & 172.17.0.1 when running in GitHub action
+    baseUrl: 'http://172.17.0.1:3000',
+  },
   lostPixelProjectId: 'clde9r3rh00v3m50vlq8y0k78',
   apiKey: process.env.LOST_PIXEL_API_KEY,
 };
